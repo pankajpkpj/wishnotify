eb4a5d1a9b54cae7a3ae347e05a6d9cbd5bee647
Pankaj Yadav <35138119+pankajpkpj@users.noreply.github.com>
2026-05-03 01:15:13 +0530

    Update middleware.ts

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -2,7 +2,9 @@ import { createServerClient } from '@supabase/ssr'
 import { NextResponse, type NextRequest } from 'next/server'
 
 export async function updateSession(request: NextRequest) {
-  let supabaseResponse = NextResponse.next({ request })
+  let supabaseResponse = NextResponse.next({
+    request,
+  })
 
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
@@ -16,7 +18,9 @@ export async function updateSession(request: NextRequest) {
           cookiesToSet.forEach(({ name, value, options }) =>
             request.cookies.set(name, value)
           )
-          supabaseResponse = NextResponse.next({ request })
+          supabaseResponse = NextResponse.next({
+            request,
+          })
           cookiesToSet.forEach(({ name, value, options }) =>
             supabaseResponse.cookies.set(name, value, options)
           )
@@ -25,6 +29,8 @@ export async function updateSession(request: NextRequest) {
     }
   )
 
+  // Refresh the session if expired - important!
   await supabase.auth.getUser()
+
   return supabaseResponse
 }
